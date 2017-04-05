15876a8da51baeeb83bc5d45f42ddec75453c005
Ilya Beda <ir4y.ix@gmail.com>
2017-04-05 22:10:47 +0700

    Add task 1.2.8

---
+++ Task_1_2_8.hs
@@ -0,0 +1,5 @@
+module Task_1_2_8 where
+
+divideList' :: (Show a, Fractional a) => [a] -> (String,a)
+divideList' []     = ("1.0", 1)
+divideList' (x:xs) = (/) <$> ("<-" ++(show x) ++ "/",x) <*> (divideList' xs)
